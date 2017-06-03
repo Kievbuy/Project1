1730d9d7a1c672ce8b6a3cac2b1c1b5001fa9a36
Dmytro Kharytonenko <kievbuy@gmail.com>
2017-06-03 17:00:48 +0300

    first comm

---
+++ project1.rb
@@ -0,0 +1,44 @@
+def sec2dhms(secs)
+  time = secs.round          # Get rid of microseconds
+  sec = time % 60            # Extract seconds
+  time /= 60                 # Get rid of seconds
+  mins = time % 60           # Extract minutes
+  time /= 60                 # Get rid of minutes
+  hrs = time % 24            # Extract hours
+  time /= 24                 # Get rid of hours
+  days = time                # Days (final remainder)
+  [days, hrs, mins, sec]     # Return array [d,h,m,s]
+end
+
+puts "Please, enter first date (Unix timestamp): "
+t1 = gets.chomp!.to_i
+puts "Please, enter second date (Unix timestamp): "
+t2 = gets.chop!.to_i
+x = t1-t2
+t = sec2dhms(x)        
+
+if t[0] == 1
+  puts "#{t[0]} day,"         # when single
+else
+  puts "#{t[0]} days,"        # when multiple
+end
+
+if t[1] == 1
+  puts "#{t[1]} hour,"         # when single
+else
+  puts "#{t[1]} hours,"        # when multiple
+end
+
+if t[2] == 1
+  puts "#{t[2]} minute,"         # when single
+else
+  puts "#{t[2]} minutes,"        # when multiple
+end
+  
+if t[3] == 1
+  puts "and #{t[3]} second."         # when single
+elsif t[3] == 0
+  print ""                          # when zero
+else
+  puts "and #{t[3]} seconds."        # when multiple
+end
